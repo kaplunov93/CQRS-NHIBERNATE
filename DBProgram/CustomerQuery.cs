af5711335291c7b844099bc1e17a2dcad7c50174
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-31 18:49:04 +0300

    Working Customer Read Logic

---
+++ DBProgram/CustomerQuery.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBProgram.Model;
+using NHibernate;
+
+namespace DBProgram
+{
+    public static class CustomerQuery
+    {
+        public static Customer FindById(int id)
+        {
+            Customer customer=null;
+            using (ISession session = SessionFactory.GetFactory().OpenSession())
+            {
+                var query = session.CreateQuery("from Customer where id= :ID");
+                query.SetParameter("ID", id);
+                customer =query.List<Customer>().First();
+            }
+            return customer;
+        }
+
+        public static IList<Customer> FindByFirstName(string name)
+        {
+            IList<Customer> customer = null;
+            using (ISession session = SessionFactory.GetFactory().OpenSession())
+            {
+                var query = session.CreateQuery("from Customer where FirstName= :Name");
+                query.SetParameter("Name", name);
+                customer = query.List<Customer>();
+            }
+            return customer;
+        }
+
+        public static IList<Customer> FindByLastName(string name)
+        {
+            IList<Customer> customer = null;
+            using (ISession session = SessionFactory.GetFactory().OpenSession())
+            {
+                var query = session.CreateQuery("from Customer where LastName= :Name");
+                query.SetParameter("Name", name);
+                customer = query.List<Customer>();
+            }
+            return customer;
+        }
+
+        public static IList<Customer> FindByNumber(string number)
+        {
+            IList<Customer> customer = null;
+            using (ISession session = SessionFactory.GetFactory().OpenSession())
+            {
+                var query = session.CreateQuery("from Customer where number= :number");
+                query.SetParameter("number", number);
+                customer = query.List<Customer>();
+            }
+            return customer;
+        }
+    }
+}
